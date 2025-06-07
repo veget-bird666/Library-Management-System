be75f16f005bb9274abcd06e3e5b204794f9f03f
shfhdfcserfw <wheazebaly@163.com>
2025-06-07 15:05:07 +0800

    Update main.ts

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,11 +1,24 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+// 注册Element Plus
+app.use(ElementPlus)
+
+// 注册所有图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+// 注册路由
 app.use(router)
 
+// 挂载应用
 app.mount('#app')
